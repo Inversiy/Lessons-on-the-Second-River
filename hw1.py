8ff3b8a6eca915cb52ffcd99d6735064e2e8b273
Inversiy <125334553+Inversiy@users.noreply.github.com>
2023-03-20 15:29:58 +1000

    Update hw1.py
    Code was improved, for instance, was added non-ascii symbols support, and was fixed some mistakes.

--- hw1.py
+++ hw1.py
@@ -27,6 +27,7 @@ def get_longest_diverse_words(file_path: str) -> List[str]:
     with open(file_path, "r") as file:
         part_of_word = ""
         for line in file:
+            line = line.encode('utf-8').decode('unicode-escape')
             words_line = line.split()
             split_line = line.split()
             if part_of_word != "" and split_line != []:
@@ -35,27 +36,49 @@ def get_longest_diverse_words(file_path: str) -> List[str]:
                 part_of_word = ""
             for i in (range(len(split_line))):
                 for j in split_line[i]:
-                    if j.lower() not in string.ascii_lowercase \
-                                    and j not in string.punctuation:
+                    if j.lower() in string.digits:
                         words_line.remove(split_line[i])
                         break
-                    if j == "-":
+                    if j == "-" and i == len(split_line)-1 and \
+                            split_line[i][len(split_line[i])-1] == "-":
                         part_of_word = split_line[i][:-1]
                         words_line.remove(split_line[i])
                         break
                 else:
-                    word_clear = re.match(r'((\W)*?(\w)+(\W)*?)', \
+                    word_clear = re.match(r'((\W)*((\w)+(\-)?(\w)*)(\W)*)', \
                                                     split_line[i], re.M)
                     if word_clear:
                         words_line[words_line.index(split_line[i])] = \
-                                                        word_clear.group(1)
+                                                        word_clear.group(3)
             for word in words_line:
                 words[word] = len(Counter(word))
     file.close()
     output_list = []
-    for i in range(len(list(Counter(words).most_common(10)))):
-        output_list.append(list(list(Counter(words).most_common(10))[i])[0])
-    return output_list
+    dictUS = Counter(words)
+    '''
+        It is sorting.
+    '''
+    nUC_last = list(list(dictUS.most_common(10))[9])[1]
+    dictUS = dictUS.most_common()
+    sort_mUC = {}
+    for i in range(len(dictUS)):
+        if dictUS[i][1] < nUC_last:
+            break
+        if dictUS[i][1] not in sort_mUC.keys():
+            sort_mUC[dictUS[i][1]] = []
+            sort_mUC[dictUS[i][1]].append(dictUS[i][0])
+        else:
+            for j in range(len(sort_mUC[dictUS[i][1]])):
+                if len(dictUS[i][0]) > len(sort_mUC[dictUS[i][1]][j]):
+                    sort_mUC[dictUS[i][1]].insert(j, dictUS[i][0])
+                    break
+    '''
+        Creating an output list from sorted dictionary.
+    '''
+    for k in sort_mUC.keys():
+        for j in range(len(sort_mUC[k])):
+            output_list.append(sort_mUC[k][j])
+    return output_list[:10]
 
 
 def get_rarest_char(file_path: str) -> str:
@@ -66,8 +89,8 @@ def get_rarest_char(file_path: str) -> str:
     main_count = Counter()
     with open(file_path, "r") as file:
        for line in file:
-           count = Counter(line)
-           main_count += count
+            line = line.encode('utf-8').decode('unicode-escape')
+            main_count += Counter(line)
     return list(main_count.most_common(len(main_count))[-1])[0]
 
 
@@ -79,8 +102,8 @@ def count_punctuation_chars(file_path: str) -> int:
     main_count = Counter()
     with open(file_path, "r") as file:
        for line in file:
-           count = Counter(line)
-           main_count += count
+            line = line.encode('utf-8').decode('unicode-escape')
+            main_count += Counter(line)
     count_punctuation = Counter()
     for i in main_count:
        if i in string.punctuation:
@@ -96,8 +119,7 @@ def count_non_ascii_chars(file_path: str) -> int:
     count_nonASCII = Counter()   
     with open(file_path, "r") as file:
         for line in file:
-           count = Counter(re.findall(r'(\\u{1}\w{4})', line, re.M))
-           count_nonASCII += count
+           count_nonASCII += Counter(re.findall(r'(\\u{1}\w{4})', line, re.M))
         return sum(count_nonASCII[x] for x in count_nonASCII)
 
 
@@ -109,7 +131,7 @@ def get_most_common_non_ascii_char(file_path: str) -> str:
     count_nonASCII = Counter()   
     with open(file_path, "r") as file:
         for line in file:
-           count = Counter(re.findall(r'(\\u{1}\w{4})', line, re.M))
-           count_nonASCII += count
+           count_nonASCII += Counter(re.findall(r'(\\u{1}\w{4})', line, re.M))
     return list(count_nonASCII.most_common(1)[0])[0]
+
 
